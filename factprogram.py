75c37b577a54cf3e87b545426c547f2c951179f8
JANISHARIFF <janisharif8@gmail.com>
2024-07-31 13:24:38 -0500

    made changes in print statements

--- factprogram.py
+++ factprogram.py
@@ -5,7 +5,8 @@ def factorial(n):
     else:
         return n * factorial(n-1)   
 n = 5 if n is None else n  # Replace with your desired number for testing
-print("Factorial of", n, "is", factorial(n))
+print("the factorial of the number " n, "is", factorial(n))
+
 
 
 
